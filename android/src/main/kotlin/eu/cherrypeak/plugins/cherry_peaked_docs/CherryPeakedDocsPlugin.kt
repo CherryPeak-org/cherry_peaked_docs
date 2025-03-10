80f624e38db140db4c247678af01d8e63c58cdf1
Samuel Kubinský <kubinsky.s@gmail.com>
2025-03-10 11:42:53 +0100

    initial Android implementation

--- android/src/main/kotlin/eu/cherrypeak/plugins/cherry_peaked_docs/CherryPeakedDocsPlugin.kt
+++ android/src/main/kotlin/eu/cherrypeak/plugins/cherry_peaked_docs/CherryPeakedDocsPlugin.kt
@@ -1,33 +1,175 @@
 package eu.cherrypeak.plugins.cherry_peaked_docs
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_JPEG
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.SCANNER_MODE_FULL
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry
+import java.io.File
+import java.io.FileOutputStream
+import java.util.UUID
 
-/** CherryPeakedDocsPlugin */
-class CherryPeakedDocsPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class CherryPeakedDocsPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
+  companion object {
+    private const val SCANNER_REQUEST_CODE = 12345
+  }
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  private lateinit var channel: MethodChannel
+  private var result: Result? = null
+  private var dirPath: String? = null
+  private var activity: Activity? = null
+
+  // ---------------------------
+  // Bridge
+  // ---------------------------
+
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "cherry_peaked_docs")
     channel.setMethodCallHandler(this)
   }
 
+  override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+    result = null
+  }
+
   override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    this.result = result
+
+    when (call.method) {
+      "startScanning" -> startScanning(call)
+      "stopScanning" -> stopScanning()
+      else -> result.notImplemented()
     }
   }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+  // ---------------------------
+  // Native
+  // ---------------------------
+
+  private fun startScanning(call: MethodCall) {
+    dirPath = call.argument<String>("path")
+    val pageLimit = call.argument<Int>("androidPageLimit")
+    val isGalleryImportAllowed = call.argument<Boolean>("isAndroidGalleryImportAllowed")
+
+    if (dirPath == null || pageLimit == null || isGalleryImportAllowed == null) {
+      result?.error("INVALID_ARGUMENTS",  "Missing or malformed arguments", null)
+      return
+    }
+
+    val optionsBuilder = GmsDocumentScannerOptions.Builder()
+      .setScannerMode(SCANNER_MODE_FULL)
+      .setResultFormats(RESULT_FORMAT_JPEG)
+      .setGalleryImportAllowed(isGalleryImportAllowed)
+
+    if (pageLimit > 0) {
+      optionsBuilder.setPageLimit(pageLimit)
+    }
+
+    GmsDocumentScanning
+      .getClient(optionsBuilder.build())
+      .getStartScanIntent(activity!!)
+      .addOnSuccessListener { intent ->
+        activity?.startIntentSenderForResult(intent, SCANNER_REQUEST_CODE, null, 0, 0, 0)
+      }
+      .addOnFailureListener { e ->
+        result?.error("SCANNER_FAILED", e.localizedMessage, e.cause?.localizedMessage)
+      }
+  }
+
+  private fun stopScanning() {
+    activity?.finishActivity(SCANNER_REQUEST_CODE)
+  }
+
+  private fun savePageFromUri(uri: Uri): String? {
+    try {
+      val inputStream = activity?.contentResolver?.openInputStream(uri)
+      val directory = File(dirPath!!)
+
+      if (!directory.exists()) {
+        directory.mkdirs()
+      }
+
+      val fileName = "${UUID.randomUUID()}.jpg"
+      val file = File(directory, fileName)
+      val outputStream = FileOutputStream(file)
+
+      inputStream?.use { input ->
+        outputStream.use { output ->
+          input.copyTo(output)
+        }
+      }
+
+      return file.absolutePath
+    } catch (e: Exception) {
+
+      e.printStackTrace()
+      return null
+    }
+  }
+
+  // ---------------------------
+  // Scan activity
+  // ---------------------------
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity
+    binding.addActivityResultListener(this)
+  }
+
+  override fun onDetachedFromActivity() {
+    activity = null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activity = binding.activity
+    binding.addActivityResultListener(this)
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+    if (requestCode != SCANNER_REQUEST_CODE) {
+      return false
+    }
+
+    if (resultCode != Activity.RESULT_OK || data == null) {
+      result?.success(emptyList<String>())
+      return true
+    }
+
+    // Get scanning result
+    val scanningResult = GmsDocumentScanningResult.fromActivityResultIntent(data)
+    if (scanningResult == null) {
+      result?.error("SCANNER_FAILED", "Scan result is null", null)
+      return true
+    }
+
+    val pages = scanningResult.pages
+    if (pages == null || pages.isEmpty()) {
+      result?.success(emptyList<String>())
+      return true
+    }
+
+    val imagePaths = pages
+      .mapNotNull { page -> page.imageUri }
+      .mapNotNull { uri -> savePageFromUri(uri) }
+
+    result?.success(imagePaths)
+    return true
   }
 }
--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -7,19 +7,19 @@
 // https://flutter.dev/to/integration-testing
 
 
-import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:integration_test/integration_test.dart';
 
-import 'package:cherry_peaked_docs/cherry_peaked_docs.dart';
+// import 'package:cherry_peaked_docs/cherry_peaked_docs.dart';
 
-void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+// void main() {
+//   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('getPlatformVersion test', (WidgetTester tester) async {
-    final CherryPeakedDocs plugin = CherryPeakedDocs();
-    final String? version = await plugin.getPlatformVersion();
-    // The version string depends on the host platform running the test, so
-    // just assert that some non-empty string is returned.
-    expect(version?.isNotEmpty, true);
-  });
-}
+//   testWidgets('getPlatformVersion test', (WidgetTester tester) async {
+//     final CherryPeakedDocs plugin = CherryPeakedDocs();
+//     final String? version = await plugin.getPlatformVersion();
+//     // The version string depends on the host platform running the test, so
+//     // just assert that some non-empty string is returned.
+//     expect(version?.isNotEmpty, true);
+//   });
+// }
